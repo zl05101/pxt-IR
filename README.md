02f36852e72d58e57560c0c9fc96c7720081ccb3
lz05101 <328222051@qq.com>
2020-06-18 18:05:05 +0800

    初始化工程

---
+++ README.md
@@ -0,0 +1,14 @@
+ # pxt-IR for micro-bit
+
+Extension for IR
+
+Note: IR receiver 
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
---
+++ enums.d.ts
@@ -0,0 +1,53 @@
+// Auto-generated. Do not edit.
+
+
+    declare enum Pins {
+    P0 = 3,
+    P1 = 2,
+    P2 = 1,
+    P3 = 4,
+    P4 = 5,
+    P5 = 17,
+    P6 = 12,
+    P7 = 11,
+    P8 = 18,
+    P9 = 10,
+    P10 = 6,
+    P11 = 26,
+    P12 = 20,
+    P13 = 23,
+    P14 = 22,
+    P15 = 21,
+    P16 = 16,
+    P19 = 0,
+    P20 = 30,
+    }
+
+
+    declare enum RemoteButton {
+    }
+declare namespace Mbit_IR {
+    CH_MINUS = 0xA2,
+	  CH = 0x62,
+	  CH_ADD = 0xE2,
+	  PREV = 0x22,
+	  NEXT = 0x02,
+	  PLAY = 0xC2,
+	  Minus = 0xE0,
+	  Add = 0xA8,
+	  EQ = 0x90,
+	  NUM0 = 0x68,
+	  NUM_100 = 0x98,
+	  NUM_200 = 0xB0,
+	  NUM1 = 0x30,
+	  NUM2 = 0x18,
+	  NUM3 = 0x7A,
+	  NUM4 = 0x10,
+	  NUM5 = 0x38,
+	  NUM6 = 0x5a,
+	  NUM7 = 0x42,
+	  NUM8 = 0x4A,
+	  NUM9 = 0x52,
+}
+
+// Auto-generated. Do not edit. Really.
